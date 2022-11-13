0171b8015d11c65c9e17cf9502d80cad8a6ad825
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2022-11-13 17:07:58 +0300

    Create strjoinofcells.m

---
+++ strjoinofcells.m
@@ -0,0 +1,18 @@
+You are given a cell array of strings and a string delimiter. You need to produce one string which is composed of each string from the cell array separated by the delimiter.
+For example, this input
+ in_cell = {'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur'};
+ delim = ' ';
+should produce this output:
+ out_str = 'Lorem ipsum dolor sit amet consectetur';
+
+
+function out_str = cellstr_joiner(in_cell, delim)
+  out_str = strjoin(in_cell,delim)
+end
+
+
+% Use this area as a scratchpad to test code that you don't want to submit:
+in_cell = {'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur'};
+delim = ' ';
+% Or call your function:
+cellstr_joiner(in_cell, delim)
